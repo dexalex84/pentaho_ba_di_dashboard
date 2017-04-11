ff47928d448a248a8b79bb01bb8f8b3f516c18fd
Burenin Aleksey <dexalex84@gmail.com>
2017-04-12 01:34:31 +0700

    last

--- init/db_dwh/create_tables.sql
+++ init/db_dwh/create_tables.sql
@@ -21,7 +21,7 @@ create table if not exists public.curr_rates
         datetime timestamp without time zone,
         curr_id_from int references public.currencies(id),
         curr_id_to int references public.currencies(id),
-        val float
+        val numeric(15,7)
 );
 
 create unique index if not exists in_curr_rates_id on public.curr_rates using btree
--- prepare_environment.sh
+++ prepare_environment.sh
@@ -40,14 +40,27 @@
  docker-compose exec  db_dwh psql -h localhost -U postgres -f "/tmp/create_database.sql"
  docker-compose exec  db_dwh psql -h localhost -U postgres -d warehouse -f "/tmp/create_tables.sql"
  echo END
+
+ echo copy Pentaho BA Dashboards to server
+ docker cp init/app_pentaho_ba/dashboards/ app_pentaho_ba:/tmp/
+
+ docker-compose exec app_pentaho_ba  ./import-export.sh --import --url=http://app_pentaho_ba:8080/pentaho --username=admin --password=password --charset=UTF-8 --path=/public --file-path=/tmp/dashboards/Currencies.cda.zip --overwrite=true --permission=true --retainOwnership=true
+
+ docker-compose exec app_pentaho_ba  ./import-export.sh --import --url=http://app_pentaho_ba:8080/pentaho --username=admin --password=password --charset=UTF-8 --path=/public --file-path=/tmp/dashboards/Currencies.cdfde.zip --overwrite=true --permission=true --retainOwnership=true
+
+ docker-compose exec app_pentaho_ba  ./import-export.sh --import --url=http://app_pentaho_ba:8080/pentaho --username=admin --password=password --charset=UTF-8 --path=/public --file-path=/tmp/dashboards/Currencies.wcdf.zip --overwrite=true --permission=true --retainOwnership=true
+
  echo 
 
  echo ===============================================================================	
  echo Pentaho BA: 
  echo ===========================
  echo Start server by command:
- echo docker-compose exec app_pentaho_ba /opt/pentaho/pentaho-server/start-pentaho.sh
+ echo docker-compose exec -T app_pentaho_ba /opt/pentaho/pentaho-server/start-pentaho.sh
  echo 
+ echo View Pentaho BA / Tomcat logs
+ echo docker-compose exec app_pentaho_ba tail -f tomcat/logs/catalina.out
+ echo
  echo Source System App:
  echo ===========================
  echo Start source server app by command:
@@ -56,6 +69,9 @@
  echo and stop by commmand:
  echo docker-compose exec -d -T app_source_system /opt/curr_rates_loader/stop.sh
  echo 
+ echo View app logs
+ echo docker-compose exec app_source_system  tail -f web_parse.log
+ echo
  echo Pentaho Kettle Integration:
  echo ===========================
  echo start Pentaho Kettle Integration job by command:
@@ -63,6 +79,11 @@
  echo 
  echo and stop by command
  echo docker-compose exec -T app_pentaho_di /opt/pentaho/app/stop.sh
+ echo 
+ echo View kettle log
+ echo docker-compose exec app_pentaho_di tail -f ../app/kettle.log
+ echo 
+ echo ===============================================================================
 
 
 
